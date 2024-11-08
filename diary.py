c29a54825a0f44947672a8290f2032ea63e10c74
git-psj <20211485@daejin.ac.kr>
2024-11-08 13:31:20 +0900

    Update diary.py

--- diary.py
+++ diary.py
@@ -5,9 +5,23 @@ from datetime import datetime
 import requests
 import json
 
-# Firebase 초기화 중복 방지
+# # secrets.toml에서 Firebase 자격 증명 정보 가져오기
+firebase_credentials = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+}
+
+# # Firebase 초기화 중복 방지
 if not firebase_admin._apps:
-    cred = credentials.Certificate('firebase_credentials.json')
+    cred = credentials.Certificate(firebase_credentials)
     firebase_admin.initialize_app(cred)
 
 # Firestore 초기화
