4d203125bc10f005b53c6c730ebd34a5b9cf06c1
git-psj <20211485@daejin.ac.kr>
2024-11-15 15:26:36 +0900

    Update solution_page.py

--- pages/solution_page.py
+++ pages/solution_page.py
@@ -6,12 +6,15 @@ from utils.etc import get_firebase_client, main
 from utils.solution_functions import display_solution_page
 
 
-# if not "id_token" in st.session_state:
-#     main()
+
 if "db" not in st.session_state:
     st.session_state.db = get_firebase_client()
 st.session_state.pwCheck = 0
 
-with st.spinner("솔루션 정보를 불러오는 중..."):
-    # 솔루션 페이지 함수 호출
-    display_solution_page()
+token , date = get_token_date()
+if not "id_token" in st.session_state and token is None:
+    main()
+else:
+    with st.spinner("솔루션 정보를 불러오는 중..."):
+        # 솔루션 페이지 함수 호출
+        display_solution_page(token , date)
