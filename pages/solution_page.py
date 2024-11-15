7698c51178ad88918a8abd20d0c1b6a141d62138
git-psj <20211485@daejin.ac.kr>
2024-11-15 15:45:25 +0900

    Update solution_page.py

--- pages/solution_page.py
+++ pages/solution_page.py
@@ -5,6 +5,9 @@ from firebase_admin import credentials, auth, firestore, storage
 from utils.etc import get_firebase_client, main
 from utils.solution_functions import display_solution_page, get_token_date
 
+if "db" not in st.session_state:
+    st.session_state.db = get_firebase_client()
+st.session_state.pwCheck = 0
 date, token = get_token_date()
 if not "id_token" in st.session_state and token is None:
     main()
