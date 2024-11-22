d109eae9e56de319ba5878cf1b8d642f8ddc0661
git-psj <20211485@daejin.ac.kr>
2024-11-23 08:58:45 +0900

    Update diary_functions.py

--- utils/diary_functions.py
+++ utils/diary_functions.py
@@ -149,13 +149,15 @@ def diary_popup(selected_date):
                 detail_page_url = f"/solution_page?{query_params}"
                 st.markdown(
                     f"""
-                    <a href="{detail_page_url}" target="_self" style="text-align: center;">
-                        <button style="background-color: #white; color: gray; border: 2px solid gray; border-radius: 10px; 
-                        margin: 5px auto; padding: 10px 20px; text-align: center; width:80%; 
-                        text-decoration: none; font-size: 15px; cursor: pointer;">
-                            결과 조회하러가기
-                        </button>
-                    </a>
+                    <div style="text-align: center;">
+                        <a href="{detail_page_url}" target="_self">
+                            <button style="background-color: white; color: gray; border: 2px solid gray; border-radius: 10px; 
+                            margin: 5px auto; padding: 10px 20px; text-align: center;
+                            text-decoration: none; font-size: 15px; cursor: pointer;">
+                                결과 조회하러가기
+                            </button>
+                        </a>
+                    </div>
                     """, unsafe_allow_html=True)
             else:
                 # 내용 입력
