f3c2a2f0a7752a0378970d01853a10b3f4a5ee00
git-psj <20211485@daejin.ac.kr>
2024-11-23 09:26:12 +0900

    Update solution_page.py

--- pages/solution_page.py
+++ pages/solution_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import firebase_admin
 from firebase_admin import credentials, auth, firestore, storage
-from utils.etc import get_firebase_client, main
+from utils.etc import get_firebase_client, main, go_logout
 from utils.solution_functions import display_solution_page, get_token_date
 
 if "db" not in st.session_state:
@@ -12,6 +12,7 @@ date, token = get_token_date()
 if not "id_token" in st.session_state and token is None:
     main()
 else:
+    go_logout()
     with st.spinner("솔루션 정보를 불러오는 중..."):
         # 솔루션 페이지 함수 호출
         display_solution_page(date, token)
