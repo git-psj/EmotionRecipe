3a6d5a7dfa752b76268aa399dc895271f207e506
git-psj <20211485@daejin.ac.kr>
2024-11-23 09:24:52 +0900

    Update show_emotion.py

--- pages/show_emotion.py
+++ pages/show_emotion.py
@@ -2,7 +2,8 @@ import streamlit as st
 from datetime import datetime, timedelta
 from urllib.parse import urlencode
 from utils.diary_functions import showDiaries
-from utils.etc import get_firebase_client, main
+from utils.etc import get_firebase_client, main, go_logout
+
 from utils.emotion_functions import plot_emotion_data
 
 if "db" not in st.session_state:
@@ -15,7 +16,7 @@ if not "id_token" in st.session_state:
 else:
     st.title("감정 레시피")
     st.markdown("<h3 style='color: gray; margin-top: -10px;'>&nbsp;- 감정통계보기</h3>", unsafe_allow_html=True)
-
+    go_logout()
     # 보기 옵션을 탭으로 구성
     tab1, tab2, tab3 = st.tabs(["주간 보기", "월별 보기", "연도별 보기"])
 
