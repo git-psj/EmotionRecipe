aa4aead42e947b5b5f9974469b2c23657a00820d
git-psj <20211485@daejin.ac.kr>
2024-11-28 09:46:40 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from utils.solution_functions import get_activity_details
 
-activities_ref = st.session_state.db.collection('activities').stream()
-st.write(activities_ref)
-st.write("---")
-activity = get_activity_details("취미 활동하기")
-st.write(activity)
+# activities_ref = st.session_state.db.collection('activities').stream()
+# st.write(activities_ref)
+# st.write("---")
+# activity = get_activity_details("취미 활동하기")
+# st.write(activity)
+docs = st.session_state.db.collection('activities').stream()
+for doc in docs:
+    st.write(f"{doc.id} => {doc.to_dict()}")
+
