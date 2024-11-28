0ad1a111a009fbd9ec1a427117173a0166aa7597
git-psj <20211485@daejin.ac.kr>
2024-11-28 11:18:07 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -4,27 +4,29 @@ from utils.solution_functions import get_activity_details
 # activities_ref = st.session_state.db.collection('activities').stream()
 # st.write(activities_ref)
 # st.write("---")
-# activity = get_activity_details("취미 활동하기")
-# st.write(activity)
-docs = st.session_state.db.collection('activities').stream()
-for doc in docs:
-    st.write(f"{doc.id} => {doc.to_dict()}")
+activity = get_activity_details("취미 활동하기")
+st.write(activity)
 
-st.write("---")
 
-docs = st.session_state.db.collection('activitiesDetail').stream()
-for doc in docs:
-    st.write(f"{doc.id} =>")
+# docs = st.session_state.db.collection('activities').stream()
+# for doc in docs:
+#     st.write(f"{doc.id} => {doc.to_dict()}")
 
-st.write("---")
+# st.write("---")
 
-docs = st.session_state.db.collection('activitiesDetail').document("취미활동하기").collection("sub_activities").stream()
-for doc in docs:
-    st.write(f"{doc.id} => s{doc.to_dict()}")
+# docs = st.session_state.db.collection('activitiesDetail').stream()
+# for doc in docs:
+#     st.write(f"{doc.id} =>")
 
-st.write("---")
-docs = st.session_state.db.collection('activitiesDetail').stream()
+# st.write("---")
 
-# 각 문서의 ID 출력
-for doc in docs:
-    st.write(f"문서 ID: {doc.id}")
+# docs = st.session_state.db.collection('activitiesDetail').document("취미활동하기").collection("sub_activities").stream()
+# for doc in docs:
+#     st.write(f"{doc.id} => s{doc.to_dict()}")
+
+# st.write("---")
+# docs = st.session_state.db.collection('activitiesDetail').stream()
+
+# # 각 문서의 ID 출력
+# for doc in docs:
+#     st.write(f"문서 ID: {doc.id}")
