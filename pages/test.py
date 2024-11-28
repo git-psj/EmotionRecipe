caf527f8cc6ad469159ad732dc972008f016e77c
git-psj <20211485@daejin.ac.kr>
2024-11-28 09:10:21 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.soultion_functions import get_activity_details
+from utils.solution_functions import get_activity_details
 
 
 activity = get_activity_details("취미 활동하기")
