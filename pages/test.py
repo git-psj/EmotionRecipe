0e3ce73d9a09bc1282c50e520700756988467199
git-psj <20211485@daejin.ac.kr>
2024-11-28 09:48:00 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -6,6 +6,12 @@ from utils.solution_functions import get_activity_details
 # st.write("---")
 # activity = get_activity_details("취미 활동하기")
 # st.write(activity)
+docs = st.session_state.db.collection('activities').stream()
+for doc in docs:
+    st.write(f"{doc.id} => {doc.to_dict()}")
+
+st.write("---"
+
 docs = st.session_state.db.collection('activitiesDetail').stream()
 for doc in docs:
     st.write(f"{doc.id} => {doc.to_dict()}")
