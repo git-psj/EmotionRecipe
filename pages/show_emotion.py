994a5c30f1e55fc6dc364e2fee4427cc6322829b
git-psj <20211485@daejin.ac.kr>
2024-12-03 17:37:15 +0900

    Update show_emotion.py

--- pages/show_emotion.py
+++ pages/show_emotion.py
@@ -72,14 +72,14 @@ else:
 
         # 이전, 다음 달 버튼
         if st.button("이전 달", key="previous_month"):
-            prev_month_end = start_date - timedelta(days=1)
+            prev_month_end -= start_date - timedelta(days=1)
             st.session_state.current_month_start = prev_month_end.replace(day=1)
             st.rerun()
         if st.button("이번달", key="current_month"):
             del st.session_state['current_month_start']
             st.rerun()
         if st.button("다음 달", key="next_month", disabled=next_month_disabled):
-            next_month_start = (end_date + timedelta(days=1)).replace(day=1)
+            next_month_start += (end_date + timedelta(days=1)).replace(day=1)
             st.session_state.current_month_start = next_month_start
             st.rerun()
 
@@ -99,13 +99,13 @@ else:
 
         # 이전, 다음 연도 버튼
         if st.button("이전 연도", key="previous_year"):
-            st.session_state.current_year_start = start_date.replace(year=start_date.year - 1)
+            st.session_state.current_year_start -= start_date.replace(year=start_date.year - 1)
             st.rerun()
         if st.button("이번 연도", key="current_year"):
             del st.session_state['current_year_start']
             st.rerun()
         if st.button("다음 연도", key="next_year", disabled=next_year_disabled):
-            st.session_state.current_year_start = start_date.replace(year=start_date.year + 1)
+            st.session_state.current_year_start += start_date.replace(year=start_date.year + 1)
             st.rerun()
 
         st.write(f"{start_date.strftime('%Y-%m-%d')} ~ {end_date.strftime('%Y-%m-%d')}")
