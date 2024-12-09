b6e4ead9353f570d9c6c01d4aa6c4d2a63ce6cbb
git-psj <20211485@daejin.ac.kr>
2024-12-10 07:37:07 +0900

    Update solution_page.py

--- pages/solution_page.py
+++ pages/solution_page.py
@@ -9,10 +9,12 @@ if "db" not in st.session_state:
     st.session_state.db = get_firebase_client()
 st.session_state.pwCheck = 0
 date, token = get_token_date()
+if "solution_date" not in st.session_state:
+    st.session_state.solution_date = date
 if not "id_token" in st.session_state and token is None:
     main()
 else:
     go_logout()
     with st.spinner("솔루션 정보를 불러오는 중..."):
         # 솔루션 페이지 함수 호출
-        display_solution_page(date, token)
+        display_solution_page(st.session_state.solution_date, token)
