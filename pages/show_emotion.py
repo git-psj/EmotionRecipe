313f3d19b86f5363d020b22108f9c848d0364fed
git-psj <20211485@daejin.ac.kr>
2024-12-10 09:48:14 +0900

    Update show_emotion.py

--- pages/show_emotion.py
+++ pages/show_emotion.py
@@ -8,6 +8,25 @@ if "db" not in st.session_state:
     st.session_state.db = get_firebase_client()
 st.session_state.pwCheck = 0
 
+def buttons(day):
+    c1, c2, c3 = st.columns([1, 3, 1])
+    with c1:
+        # 이전 버튼
+        if st.button("이전", key="previous"):
+            st.session_state.emotion_date -= timedelta(days=day)
+            st.rerun()
+    with c2:
+        # 날짜 선택 위젯
+        emotion_date = st.date_input("날짜 선택", value=st.session_state.emotion_date, label_visibility="collapsed")
+        if emotion_date != st.session_state.emotion_date:
+            st.session_state.emotion_date = emotion_date        
+            st.rerun()  # 날짜가 변경되면 페이지를 새로 고침      
+    with c3:
+        # 다음 버튼
+        if st.button("다음", key="next"):
+            st.session_state.emotion_date += timedelta(days=day)
+            st.rerun()
+
 # 로그인 확인
 if not "id_token" in st.session_state:
     main()
@@ -38,19 +57,9 @@ else:
         end_date = start_date + timedelta(days=6)
 
         # 버튼 가로 정렬
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            if st.button("이전 주", key="previous_week"):
-                st.session_state.current_week_start -= timedelta(weeks=1)
-        with col2:
-            if st.button("이번 주로 이동", key="current_week"):
-                st.session_state.current_week_start = today - timedelta(days=today.weekday())
-        with col3:
-            disabled_next_week = st.session_state.current_week_start + timedelta(weeks=1) > today
-            st.button("다음 주", key="next_week", disabled=disabled_next_week)
-            if not disabled_next_week:
-                st.session_state.current_week_start += timedelta(weeks=1)
+        buttons(start_date, 7)
 
+        
         st.write(f"{start_date.strftime('%Y-%m-%d')} ~ {end_date.strftime('%Y-%m-%d')}")
         plot_emotion_data(start_date, end_date)
 
